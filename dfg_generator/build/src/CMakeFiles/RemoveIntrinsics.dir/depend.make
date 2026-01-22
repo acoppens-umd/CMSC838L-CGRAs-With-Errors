512a8c3f5b0637fa85599affb2eedc37c91fbee8
acoppens-umd <acoppens@terpmail.umd.edu>
2026-01-21 21:20:53 -0500

    Removed build directories from tracking dfg_generator

--- dfg_generator/build/src/CMakeFiles/RemoveIntrinsics.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for RemoveIntrinsics.
-# This may be replaced when dependencies are built.
--- dfg_generator/build/src/CMakeFiles/dfggenPass.dir/common/dfgmacro.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/dfggenPass.dir/common/dfgmacro.cpp.o: \
- /home/acoppens/CMSC838L-morpher-v2/CMSC838L-CGRAs-With-Errors/dfg_generator/src/common/dfgmacro.cpp \
- /usr/include/stdc-predef.h
