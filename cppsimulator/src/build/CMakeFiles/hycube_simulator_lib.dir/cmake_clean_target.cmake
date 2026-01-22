f8a9e3b7b9210c8be7b07eb1af55bdea66e156e7
acoppens-umd <acoppens@terpmail.umd.edu>
2026-01-21 21:20:27 -0500

    Removed build directories from tracking

--- cppsimulator/src/build/CMakeFiles/hycube_simulator_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libhycube_simulator_lib.a"
-)
--- mapper/build/src/CMakeFiles/cgra_xml_mapper.dir/dfg/DFGEdge.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/cgra_xml_mapper.dir/dfg/DFGEdge.cpp.o: \
- /home/acoppens/CMSC838L-morpher-v2/CMSC838L-CGRAs-With-Errors/mapper/src/dfg/DFGEdge.cpp \
- /usr/include/stdc-predef.h \
- /home/acoppens/CMSC838L-morpher-v2/CMSC838L-CGRAs-With-Errors/mapper/include/morpher/dfg/DFGEdge.h
